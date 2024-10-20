ed64e741eb2c1324aa3a1a3b338eefd5183934c6
Sonu Singh <son797sna@gmail.com>
2024-10-20 21:44:24 +0530

    prac2 updated

--- prac2.py
+++ prac2.py
@@ -4,3 +4,7 @@ def greet_user(name: str = "sonu"):
 
 def gree_admin_user(name: str = "admin"):
     return f"Hello {name}"
+
+
+def greet_sub_admin(name: str = "subadmin"):
+    return f"Hello {name}"
